799d1109d78169dd6a56312c65fc65db8c2a7aa5
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-12-07 14:54:25 +0200

    Cross Pattern

--- cppAssignmentsPart1/coutFigures/figureCrossPattern.cpp
+++ cppAssignmentsPart1/coutFigures/figureCrossPattern.cpp
@@ -10,30 +10,34 @@ using namespace std;
 
 int main()
 {
-    int rows;
-    cout << "How many rows do you want the pattern to be?:" << endl;
-    cin >> rows;
-
-    for (int i = 1; i <= rows; i++) // Създаваме вертикалните * на формата.
+    while (true)
     {
-        for (int align = 2; align >= 1; align--) // "Избутваме" * на всеки 1ви и 2ри ред, за да спазим формата на кръст.
-        {
-            if (i % 3 != 0)
-            {
-                cout << ' ';
-            }
-        }
 
-        if (i % 3 == 0) // На всеки 3ти ред принтираме допълнително по 4 * на ред, за да спазим формата на кръст .
-        {
-            for (int j = 1; j <= 4; j++)
-            {
-                cout << '*';
-            }
-        }
+        int rows;
+        cout << "How many rows do you want the pattern to be?:" << endl;
+        cin >> rows;
+        system("cls");
 
-        cout << '*' << endl;
+        for (int i = 1; i <= rows; i++) // Създаваме вертикалните * на формата.
+        {
+            for (int align = 2; align >= 1; align--) // "Избутваме" * на всеки 1ви и 2ри ред, за да спазим формата на кръст.
+            {
+                if (i % 3 != 0)
+                {
+                    cout << ' ';
+                }
+            }
+
+            if (i % 3 == 0) // На всеки 3ти ред принтираме допълнително по 4 * на ред, за да спазим формата на кръст .
+            {
+                for (int j = 1; j <= 4; j++)
+                {
+                    cout << '*';
+                }
+            }
+
+            cout << '*' << endl;
+        }
     }
-
     return 0;
 }
