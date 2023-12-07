c1a29546e5d02c9ce8ebdf326cd4f337c5461946
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-12-07 14:57:43 +0200

    Diamond

--- cppAssignmentsPart1/coutFigures/figureDiamond.cpp
+++ cppAssignmentsPart1/coutFigures/figureDiamond.cpp
@@ -14,44 +14,49 @@ using namespace std;
 
 int main()
 {
-    int rows;
-    cout << "How many rows do you want the diamond to be? Enter an odd number:" << endl;
-    cin >> rows;
-
-    while (rows % 2 == 0) // Проверяваме дали въведеният брой редове е четен.
+    while (true)
     {
-        cout << "Enter an odd number:" << endl;
+
+        int rows;
+        cout << "How many rows do you want the diamond to be? Enter an odd number:" << endl;
         cin >> rows;
+
+        while (rows % 2 == 0) // Проверяваме дали въведеният брой редове е четен.
+        {
+            cout << "Enter an odd number:" << endl;
+            cin >> rows;
+        }
+        
+        system("cls");
+
+        // FOR1 (increment): подравняваме горната част на диаманта (под формата на пирамида).
+        for (int i = 1; i <= rows; i += 2)
+        {
+            for (int align = rows - 1; align >= i; align -= 2)
+            {
+                cout << " ";
+            }
+
+            for (int j = 1; j <= i; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+
+        // FOR2 (decrement): Създаваме долните редове на диаманта (под формата на обърната пирамида), като пропускаме най-дългия ред.
+        for (int i = rows - 2; i >= 1; i -= 2)
+        {
+            for (int align = rows - 1; align >= i; align -= 2)
+            {
+                cout << " ";
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
     }
-
-    // FOR1 (increment): подравняваме горната част на диаманта (под формата на пирамида).
-    for (int i = 1; i <= rows; i += 2)
-    {
-        for (int align = rows - 1; align >= i; align -= 2)
-        {
-            cout << " ";
-        }
-
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    // FOR2 (decrement): Създаваме долните редове на диаманта (под формата на обърната пирамида), като пропускаме най-дългия ред.
-    for (int i = rows - 2; i >= 1; i -= 2)
-    {
-        for (int align = rows - 1; align >= i; align -= 2)
-        {
-            cout << " ";
-        }
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
     return 0;
 }
