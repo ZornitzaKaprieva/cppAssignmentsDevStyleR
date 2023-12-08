914025e9466bcfac3663922c713f3177e2d657bb
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-12-08 12:56:26 +0200

    Cross Pattern

--- cppAssignmentsPart1/coutFigures/figureCrossPattern.cpp
+++ cppAssignmentsPart1/coutFigures/figureCrossPattern.cpp
@@ -12,15 +12,17 @@ int main()
 {
     while (true)
     {
-
         int rows;
-        cout << "How many rows do you want the pattern to be?:" << endl;
+        char symbol;
+        cout << "How many rows do you want the pattern to be?: ";
         cin >> rows;
+        cout << "Which symbol do you want to use in the pattern?: ";
+        cin >> symbol;
         system("cls");
 
-        for (int i = 1; i <= rows; i++) // Създаваме вертикалните * на формата.
+        for (int i = 1; i <= rows; i++) // Създаваме вертикалните символи на формата.
         {
-            for (int align = 2; align >= 1; align--) // "Избутваме" * на всеки 1ви и 2ри ред, за да спазим формата на кръст.
+            for (int align = 2; align >= 1; align--) // "Избутваме" символите на всеки 1ви и 2ри ред, за да спазим формата на кръст.
             {
                 if (i % 3 != 0)
                 {
@@ -28,15 +30,15 @@ int main()
                 }
             }
 
-            if (i % 3 == 0) // На всеки 3ти ред принтираме допълнително по 4 * на ред, за да спазим формата на кръст .
+            if (i % 3 == 0) // На всеки 3ти ред принтираме допълнително по 4 символа на ред, за да спазим формата на кръст .
             {
                 for (int j = 1; j <= 4; j++)
                 {
-                    cout << '*';
+                    cout << symbol;
                 }
             }
 
-            cout << '*' << endl;
+            cout << symbol << endl;
         }
     }
     return 0;
