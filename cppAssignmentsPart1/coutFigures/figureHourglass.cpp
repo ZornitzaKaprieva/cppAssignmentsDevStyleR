ff6cff83e33bce3774ecf9efb9cb548db19e02d5
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-12-08 13:04:26 +0200

    Hourglass

--- cppAssignmentsPart1/coutFigures/figureHourglass.cpp
+++ cppAssignmentsPart1/coutFigures/figureHourglass.cpp
@@ -12,12 +12,16 @@ int main()
 {
     while (true)
     {
+
         int rows;
-        cout << "How many rows do you want the hourglass to be?:" << endl;
+        char symbol;
+        cout << "How many rows do you want the hourglass to be?: ";
         cin >> rows;
+        cout << "Which symbol do you want to use in the pattern?: ";
+        cin >> symbol;
         system("cls");
 
-        int doubleRows = rows * 2;                   // Удвояваме стойността, получена в rows, тъй * са нечетни на всеки ред => са по-голям брой от броя редове.
+        int doubleRows = rows * 2;                   // Удвояваме стойността, получена в rows, тъй като символите са нечетен брой на всеки ред => са по-голям брой от броя редове.
         for (int i = doubleRows - 1; i >= 1; i -= 2) // 1. Създаваме редовете на обърнатата пирамида, като правим декрементацията през 2
         {
 
@@ -26,12 +30,12 @@ int main()
                 cout << " ";
             }
 
-            for (int j = 1; j <= i; j++) // 3. Попълваме създадените редове със *.
+            for (int j = 1; j <= i; j++) // 3. Попълваме създадените редове със символи.
             {
-                cout << "*";
+                cout << symbol;
             }
 
-            cout << endl; // Ако не поставим нов ред след всяка итерация i, всички * ще се изпишат на един ред, а не под формата на пирамида
+            cout << endl; // Ако не поставим нов ред след всяка итерация i, всички символи ще се изпишат на един ред, а не под формата на пирамида
         }
     }
     return 0;
